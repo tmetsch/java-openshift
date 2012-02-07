6968d4fac9e05bfd8e108dbd70806cd16e8b1377
unknown <tmetsch@.(none)>
2012-02-07 22:10:15 +0100

    first shot on mini rest app

---
+++ src/main/java/the/ultimate/test/pkg/HelloServlet.java
@@ -0,0 +1,109 @@
+package the.ultimate.test.pkg;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Simple REST Hello World Service.
+ *
+ * @author tmetsch
+ *
+ */
+public class HelloServlet extends HttpServlet {
+
+    /**
+     * A serial UID.
+     */
+    private static final long serialVersionUID = -2345771005431135205L;
+
+    /**
+     * List of names.
+     */
+    private List<String> users = null;
+
+    /**
+     * Constructor.
+     */
+    public HelloServlet() {
+        users = new LinkedList<String>();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String path = req.getContextPath().substring(1);
+
+        System.out.println("size" + users.size());
+        for (String item : users) {
+            System.out.println(item);
+        }
+
+        PrintWriter out = resp.getWriter();
+        if (path.equals("")) {
+            for (String name : users) {
+                out.write(name);
+            }
+        } else if (users.contains(path)) {
+            out.write("Hello " + path);
+        } else {
+            resp.sendError(404);
+        }
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        PrintWriter out = new PrintWriter(resp.getWriter());
+
+        // extract data
+        if (req.getContentType().equals("text/plain")) {
+            String data = readBody(req).toLowerCase();
+            System.out.println("body was:" + data);
+            users.add(data);
+            resp.setStatus(200);
+            out.write(data);
+        } else {
+            resp.sendError(406,
+                    "Content-Type not defined or unknown - needs to be" +
+                    " text/plain...");
+        }
+    }
+
+    /**
+     * Reads the HTTP body from the request.
+     *
+     * @param request
+     *            The HTTPServlet request.
+     * @return A String.
+     * @throws IOException
+     *             On error.
+     */
+    private final String readBody(final HttpServletRequest request)
+            throws IOException {
+        InputStream in = request.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(in));
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line + "\n");
+        }
+
+        bufferedReader.close();
+        return stringBuilder.toString().replace("\n", "");
+    }
+}
---
+++ src/test/java/the/ultimate/test/pkg/HelloServletTest.java
@@ -0,0 +1,229 @@
+package the.ultimate.test.pkg;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import junit.framework.TestCase;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+
+/**
+ * Unittest for the HelloServlet.
+ *
+ * @author tmetsch
+ *
+ */
+public class HelloServletTest extends TestCase{
+
+    /**
+     * Mock context.
+     */
+    private Mockery context = new Mockery();
+
+    /**
+     * req mock.
+     */
+    private HttpServletRequest req = context.mock(HttpServletRequest.class);
+
+    /**
+     * resp mock.
+     */
+    private HttpServletResponse resp = context.mock(HttpServletResponse.class);
+
+    /**
+     * test instance.
+     */
+    private HelloServlet testClass = new HelloServlet();
+
+
+    /*
+     * Test for success
+     */
+
+    /**
+     * Test method for doGet.
+     */
+    public void testDoGetForSuccess() {
+        try {
+            context.checking(new Expectations() {
+                {
+                    oneOf(req).getContextPath();
+                    will(returnValue("/"));
+
+                    oneOf(resp).getWriter();
+                    will(returnValue(new PrintWriter(System.out, true)));
+                }
+            });
+            testClass.doGet(req, resp);
+            context.assertIsSatisfied();
+        } catch (ServletException e) {
+            fail(e.getMessage());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Test method for doPost.
+     */
+    public void testDoPostForSuccess() {
+        try {
+            context.checking(new Expectations() {
+                {
+                    oneOf(req).getContentType();
+                    will(returnValue("text/plain"));
+
+                    oneOf(req).getInputStream();
+                    will(returnValue(new DummyInputStream("Bob")));
+
+                    oneOf(resp).getWriter();
+                    will(returnValue(new PrintWriter(System.out, true)));
+
+                    oneOf(resp).setStatus(200);
+                }
+            });
+            testClass.doPost(req, resp);
+            context.assertIsSatisfied();
+        } catch (ServletException e) {
+            fail(e.getMessage());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /*
+     * Test for failure
+     */
+
+    /**
+     * Test method for doGet.
+     */
+    public void testDoGetForFailure() {
+        try {
+            context.checking(new Expectations() {
+                {
+                    allowing(req).getContextPath();
+                    will(returnValue("/foobar"));
+
+                    oneOf(resp).getWriter();
+                    will(returnValue(new PrintWriter(System.out, true)));
+
+                    oneOf(resp).sendError(404);
+                }
+            });
+            testClass.doGet(req, resp);
+            context.assertIsSatisfied();
+        } catch (ServletException e) {
+            fail(e.getMessage());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Test method for doPost.
+     */
+    public void testDoPostForFailure() {
+        try {
+            context.checking(new Expectations() {
+                {
+                    oneOf(req).getContentType();
+                    will(returnValue("application/json"));
+
+                    oneOf(resp).getWriter();
+                    will(returnValue(new PrintWriter(System.out, true)));
+
+                    oneOf(resp).sendError(with(any(Integer.class)),
+                            with(any(String.class)));
+                }
+            });
+            testClass.doPost(req, resp);
+            context.assertIsSatisfied();
+        } catch (ServletException e) {
+            fail(e.getMessage());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /*
+     * Test for sanity
+     */
+
+    /**
+     * Test method for doPost.
+     */
+    public void testDoPostForSanity() {
+        try {
+            context.checking(new Expectations() {
+                {
+                    oneOf(req).getContentType();
+                    will(returnValue("text/plain"));
+
+                    oneOf(req).getInputStream();
+                    will(returnValue(new DummyInputStream("Bob")));
+
+                    oneOf(resp).getWriter();
+                    will(returnValue(new PrintWriter(System.out, true)));
+
+                    oneOf(resp).setStatus(200);
+                }
+            });
+            testClass.doPost(req, resp);
+            context.assertIsSatisfied();
+
+            context.checking(new Expectations() {
+                {
+                    oneOf(req).getContextPath();
+                    will(returnValue("/bob"));
+
+                    oneOf(resp).getWriter();
+                    will(returnValue(new PrintWriter(System.out, true)));
+                }
+            });
+            testClass.doGet(req, resp);
+            context.assertIsSatisfied();
+        } catch (ServletException e) {
+            fail(e.getMessage());
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /**
+     * Innter class for testing purposes.
+     *
+     * @author tmetsch
+     *
+     */
+    protected static class DummyInputStream extends ServletInputStream {
+
+        /**
+         * A counter.
+         */
+        private InputStream reader;
+
+        /**
+         * Constructor.
+         *
+         * @param text
+         *            text to use.
+         */
+        public DummyInputStream(final String text) {
+            reader = new ByteArrayInputStream(text.getBytes());
+        }
+
+        @Override
+        public final int read() throws IOException {
+            return reader.read();
+        }
+    }
+}
